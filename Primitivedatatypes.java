ea9e29ab6adb5618092d51782acc7aa4fb7bdf2c
upendarmanikyam <upendarmanikyam93@gmail.com>
2024-05-14 14:32:29 +0530

    Add files via upload

---
+++ Primitivedatatypes.java
@@ -0,0 +1,38 @@
+package javafullstack;
+
+public class Primitivedatatypes {
+
+	public static void main(String[] args) {
+	  // byte
+		        byte byteVar = 100;
+		        System.out.println("byte: " + byteVar);
+
+		        // short
+		        short shortVar = 1000;
+		        System.out.println("short: " + shortVar);
+
+		        // int
+		        int intVar = 100000;
+		        System.out.println("int: " + intVar);
+
+		        // long
+		        long longVar = 1000000000L;
+		        System.out.println("long: " + longVar);
+
+		        // float
+		        float floatVar = 3.14f;
+		        System.out.println("float: " + floatVar);
+
+		        // double
+		        double doubleVar = 3.14159;
+		        System.out.println("double: " + doubleVar);
+
+		        // char
+		        char charVar = 'A';
+		        System.out.println("char: " + charVar);
+
+		        // boolean
+		        boolean boolVar = true;
+		        System.out.println("boolean: " + boolVar);
+		    }
+		}
