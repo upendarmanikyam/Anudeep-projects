87bc984c56b79f7e0f229cd5df6e20efa9c1be82
upendarmanikyam <upendarmanikyam93@gmail.com>
2024-05-20 17:22:48 +0530

    Add files via upload

---
+++ Lab 5/EmployeeDetails.java
@@ -0,0 +1,17 @@
+package javafullstack;
+
+public class EmployeeDetails {
+	static int id =1;
+	static String name = "Upendar" ; 
+	static String companyName = "ANUDEEP"; 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Employee ID: "+id);
+		System.out.println("Employee name: "+name);
+		System.out.println("Company Name: " + companyName); 
+		//sop(Demo.x);
+
+	}
+
+}
+
