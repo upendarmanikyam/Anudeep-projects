cf876e68746d02f4c52c8010c02dd9c37b189690
upendarmanikyam <upendarmanikyam93@gmail.com>
2024-05-29 17:04:17 +0530

    Add files via upload

---
+++ Activities/StingInbuilt.java
@@ -0,0 +1,31 @@
+package javafullstack;
+
+public class StingInbuilt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		        String s = "THALA FOR A REASON";
+		        String up = s.toUpperCase();
+		        String lo = s.toLowerCase();
+		        System.out.println( s);
+		        System.out.println( up);
+		        System.out.println(lo);
+
+		        System.out.println( s.startsWith("THALA"));
+
+		        System.out.println( s.endsWith("FOR A"));
+
+		        System.out.println( s.contains("REASON"));
+
+		        String s1 = "";
+		        System.out.println(s1.isEmpty());
+
+		        int length = s.length();
+		        System.out.println( length);
+		    }
+		
+	}
+
+
