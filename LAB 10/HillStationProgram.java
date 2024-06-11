3505b8b7a7baec460344e2b2dc04fb81b9e58555
upendarmanikyam <upendarmanikyam93@gmail.com>
2024-06-11 17:51:13 +0530

    Add files via upload

---
+++ LAB 10/HillStationProgram.java
@@ -0,0 +1,65 @@
+package javafullstack;
+
+class HillStation {
+    void location() {
+        System.out.println();
+    }
+
+    void famousFor() {
+        System.out.println();
+    }
+}
+
+class Manali extends HillStation {
+   
+    void location() {
+        System.out.println("Location: MANALI");
+    }
+
+  
+    void famousFor() {
+        System.out.println("Famous for:HILLS");
+    }
+}
+
+class Mussoorie extends HillStation {
+
+    void location() {
+        System.out.println("Location: Mussoorie");
+    }
+
+  
+    void famousFor() {
+        System.out.println("Famous for: SWEETS");
+    }
+}
+
+class Gulmarg extends HillStation {
+   
+    void location() {
+        System.out.println("Location: Gulmarg");
+    }
+
+    void famousFor() {
+        System.out.println("Famous for: GUNS");
+    }
+}
+
+public class HillStationProgram {
+    public static void main(String[] args) {
+    	// TODO Auto-generated method stub	 
+    	  
+          Manali manali = new Manali();
+          manali.location();
+          manali.famousFor();
+
+          Mussoorie mussoorie = new Mussoorie();
+          mussoorie.location();
+          mussoorie.famousFor();
+
+          Gulmarg gulmarg = new Gulmarg();
+          gulmarg.location();
+          gulmarg.famousFor();
+    }
+}
+
